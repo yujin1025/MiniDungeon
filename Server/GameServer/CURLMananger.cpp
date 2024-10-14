8669114f9ac7a7d26a260877efba005f8c6bcec6
hans4809 <58892107+hans4809@users.noreply.github.com>
2024-10-15 02:09:54 +0900

    refactoring : CURLMananger 리팩토링 중

--- Server/GameServer/CURLMananger.cpp
+++ Server/GameServer/CURLMananger.cpp
@@ -1,11 +1,11 @@
 #include "pch.h"
 #include "CURLMananger.h"
 #include <fstream>
-#include <curl/curl.h>
 #include <ctime>
 #include <sstream>
 #include <iomanip>
 #include <random>
+#include <cstring>
 
 #define FROM_ADDR "hans4809@gmail.com"// 발신자의 이메일 주소
 #define TO_ADDR "hondaestudy@gmail.com"// 수신자의 이메일 주소
@@ -117,6 +117,15 @@ static size_t payload_source(char* ptr, size_t size, size_t nmemb, void* userp)
 }
 
 
+void CURLMananger::AddAuthWaiter(const string& email, const string& password)
+{
+    WRITE_LOCK;
+	AuthInfo info;
+	info.password = password;
+	info.authNo = GenerateVerificationCode();
+	_authWaiters.insert(make_pair(email, info));
+}
+
 string CURLMananger::GenerateMessageID(const string& domain)
 {
     //time_t now = time(nullptr);
@@ -149,103 +158,100 @@ string CURLMananger::GenerateMessageID(const string& domain)
     return messageID.str();
 }
 
-string CURLMananger::SetPayLoadText()
+string CURLMananger::SetPayLoadText(const string& email, AuthInfo info)
 {
-    string return_string = "To: ";
-    //_payload_string = "To: ";
-    return_string.append(_toAddr.c_str());
-    return_string.append("\r\n");
+    _payload = "To: ";
+    _payload.append(email.c_str());
+    _payload.append("\r\n");
 
-    return_string.append("From: ");
-    return_string.append(FROM_ADDR);
-    return_string.append("\r\n");
+    _payload.append("From: ");
+    _payload.append(FROM_ADDR);
+    _payload.append("\r\n");
 
-    return_string.append("Meesage-ID: ");
+    _payload.append("Meesage-ID: ");
     _messageID = GenerateMessageID("rfcpedant.gmail.com");
-    return_string.append(_messageID.c_str());
-    return_string.append("\r\n");
+    _payload.append(_messageID.c_str());
+    _payload.append("\r\n");
 
-    return_string.append("Subject: 이메일 인증 코드\r\n");
-    return_string.append("MIME-Version: 1.0\r\n");
-    return_string.append("Content-Type: text/html; charset=UTF-8\r\n");
-    return_string.append("\r\n");
+    _payload.append("Subject: 이메일 인증 코드\r\n");
+    _payload.append("MIME-Version: 1.0\r\n");
+    _payload.append("Content-Type: text/html; charset=UTF-8\r\n");
+    _payload.append("\r\n");
 
-    return_string.append("<!DOCTYPE html>\r\n");
-    return_string.append("<html xmlns:th=\"http://www.thymeleaf.org\">");
+    _payload.append("<!DOCTYPE html>\r\n");
+    _payload.append("<html xmlns:th=\"http://www.thymeleaf.org\">");
 
-    return_string.append("<head>\r\n");
-    return_string.append("<meta charset=\"UTF-8\">\r\n");
-    return_string.append("<title>이메일 인증 코드</title>\r\n");
-    return_string.append("<style>\r\n");
-    return_string.append("body {");
-    return_string.append("font-family: Arial, sans-serif;");
-    return_string.append("background-color: #f4f4f4;");
-    return_string.append("margin: 0;");
-    return_string.append("padding: 0;}\r\n");
+    _payload.append("<head>\r\n");
+    _payload.append("<meta charset=\"UTF-8\">\r\n");
+    _payload.append("<title>이메일 인증 코드</title>\r\n");
+    _payload.append("<style>\r\n");
+    _payload.append("body {");
+    _payload.append("font-family: Arial, sans-serif;");
+    _payload.append("background-color: #f4f4f4;");
+    _payload.append("margin: 0;");
+    _payload.append("padding: 0;}\r\n");
 
-    return_string.append(".container {");
-    return_string.append("max-width: 600px;");
-    return_string.append("margin: 0 auto;");
-    return_string.append("background-color: #ffffff;");
-    return_string.append("padding: 20px;");
-    return_string.append("border: 1px solid #dddddd;}\r\n");
+    _payload.append(".container {");
+    _payload.append("max-width: 600px;");
+    _payload.append("margin: 0 auto;");
+    _payload.append("background-color: #ffffff;");
+    _payload.append("padding: 20px;");
+    _payload.append("border: 1px solid #dddddd;}\r\n");
 
-    return_string.append(".header {");
-    return_string.append("text-align: center;");
-    return_string.append("background-color: #3498db;");
-    return_string.append("padding: 20px;");
-    return_string.append("color: #ffffff;}\r\n");
+    _payload.append(".header {");
+    _payload.append("text-align: center;");
+    _payload.append("background-color: #3498db;");
+    _payload.append("padding: 20px;");
+    _payload.append("color: #ffffff;}\r\n");
 
-    return_string.append(".content {");
-    return_string.append("margin-top: 20px;");
-    return_string.append("font-size: 16px;");
-    return_string.append("line-height: 1.6;}\r\n");
+    _payload.append(".content {");
+    _payload.append("margin-top: 20px;");
+    _payload.append("font-size: 16px;");
+    _payload.append("line-height: 1.6;}\r\n");
 
-    return_string.append(".code {");
-    return_string.append("background-color: #f1f1f1;");
-    return_string.append("padding: 10px;");
-    return_string.append("font-size: 20px;");
-    return_string.append("text-align: center;");
-    return_string.append("font-weight: bold;");
-    return_string.append("margin: 20px 0;");
-    return_string.append("letter-spacing: 2px;}\r\n");
+    _payload.append(".code {");
+    _payload.append("background-color: #f1f1f1;");
+    _payload.append("padding: 10px;");
+    _payload.append("font-size: 20px;");
+    _payload.append("text-align: center;");
+    _payload.append("font-weight: bold;");
+    _payload.append("margin: 20px 0;");
+    _payload.append("letter-spacing: 2px;}\r\n");
 
-    return_string.append(".footer {");
-    return_string.append("margin-top: 20px;");
-    return_string.append("font-size: 12px;");
-    return_string.append("color: #777777;");
-    return_string.append("text-align: center;}\r\n");
+    _payload.append(".footer {");
+    _payload.append("margin-top: 20px;");
+    _payload.append("font-size: 12px;");
+    _payload.append("color: #777777;");
+    _payload.append("text-align: center;}\r\n");
 
-    return_string.append("</style>\r\n");
-    return_string.append("</head>\r\n");
+    _payload.append("</style>\r\n");
+    _payload.append("</head>\r\n");
 
-    return_string.append("<body>\r\n");
-    return_string.append("<div class=\"container\">\r\n");
-    return_string.append("<div class=\"header\">\r\n");
-    return_string.append("<h1>이메일 인증 코드</h1>\r\n");
-    return_string.append("</div>\r\n");
+    _payload.append("<body>\r\n");
+    _payload.append("<div class=\"container\">\r\n");
+    _payload.append("<div class=\"header\">\r\n");
+    _payload.append("<h1>이메일 인증 코드</h1>\r\n");
+    _payload.append("</div>\r\n");
 
-    return_string.append("<div class=\"content\">");
-    return_string.append("<p>아래의 인증 코드를 입력해주세요.</p>\r\n");
-    return_string.append("<div class=\"code\">");
-    return_string.append(GenerateVerificationCode());
-    return_string.append("</div>\r\n");
-    return_string.append("<p>이 코드는 5분간 유효합니다.</p>\r\n");
-    return_string.append("<p>감사합니다.</p>\r\n");
-    return_string.append("</div>\r\n");
-    return_string.append("<hr>\r\n");
+    _payload.append("<div class=\"content\">");
+    _payload.append("<p>아래의 인증 코드를 입력해주세요.</p>\r\n");
+    _payload.append("<div class=\"code\">");
+    _payload.append(to_string(info.authNo));
+    _payload.append("</div>\r\n");
+    _payload.append("<p>이 코드는 5분간 유효합니다.</p>\r\n");
+    _payload.append("<p>감사합니다.</p>\r\n");
+    _payload.append("</div>\r\n");
+    _payload.append("<hr>\r\n");
 
-    return_string.append("<div class=\"footer\">\r\n");
-    return_string.append("<p>이 메일은 자동으로 발송된 메일입니다. 회신하지 마세요.</p>\r\n");
-    return_string.append("</div>\r\n");
-    return_string.append("</div>\r\n");
-    return_string.append("</body>\r\n");
-    return_string.append("</html>\r\n");
-
-    return return_string;
+    _payload.append("<div class=\"footer\">\r\n");
+    _payload.append("<p>이 메일은 자동으로 발송된 메일입니다. 회신하지 마세요.</p>\r\n");
+    _payload.append("</div>\r\n");
+    _payload.append("</div>\r\n");
+    _payload.append("</body>\r\n");
+    _payload.append("</html>\r\n");
 }
 
-string CURLMananger::GenerateVerificationCode()
+int CURLMananger::GenerateVerificationCode()
 {
     // 난수 생성기 초기화
     random_device rd; // 실제 하드웨어 엔트로피 소스를 이용한 랜덤 시드
@@ -255,21 +261,29 @@ string CURLMananger::GenerateVerificationCode()
     // 6자리 인증 코드 생성
     int code = dist(generator);
 
-    // 6자리로 고정된 형식으로 문자열 생성
-    ostringstream codeStream;
-    codeStream << setw(6) << setfill('0') << code;
-
-    return codeStream.str();
+    return code;
 }
 
 CURLMananger::CURLMananger()
 {
-    _toAddr = "hondaestudy@gmail.com";
-    _messageID = GenerateMessageID("gmail.com");
+    _connection = curl_easy_init();
+    curl_easy_setopt(_connection, CURLOPT_URL, "smtps://smtp.gmail.com:465"); // 포트 465 사용
+    // 기본 이메일 설정
+    curl_easy_setopt(_connection, CURLOPT_USERNAME, FROM_ADDR);
+    curl_easy_setopt(_connection, CURLOPT_PASSWORD, PASSWORD);
+    curl_easy_setopt(_connection, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
+    // 필요 시 인증서 검증 비활성화 (테스트 환경에서만 사용)
+    curl_easy_setopt(_connection, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(_connection, CURLOPT_SSL_VERIFYHOST, 0L);
+
+    // 발신자 이메일 설정
+    curl_easy_setopt(_connection, CURLOPT_MAIL_FROM, "<hans4809@gmail.com>");
 }
 
 CURLMananger::~CURLMananger()
 {
+    curl_easy_cleanup(_connection);
 }
 
 void CURLMananger::SendMail()
@@ -341,4 +355,185 @@ void CURLMananger::SendMail()
         curl_slist_free_all(recipients);
         curl_easy_cleanup(curl);
     }
+}
+
+void CURLMananger::SendMail(const string& toAddr, const string& password)
+{
+    AddAuthWaiter(toAddr, password);
+
+    if (_connection != nullptr)
+    {
+        WRITE_LOCK;
+
+        CURLcode res;
+        struct curl_slist* recipients = NULL;
+        struct upload_status upload_ctx = { 0 };
+
+        // 수신자 이메일 설정
+        string recipient = "<";
+        recipient.append(toAddr);
+        recipient.append(">");
+        recipients = curl_slist_append(recipients, recipient.c_str());
+        curl_easy_setopt(_connection, CURLOPT_MAIL_RCPT, recipients);
+
+        if(_authWaiters.find(toAddr) != _authWaiters.end())
+		{
+			AuthInfo info = _authWaiters[toAddr];
+			SetPayLoadText(toAddr, info);
+
+			// 이메일 본문 설정
+			curl_easy_setopt(_connection, CURLOPT_READFUNCTION, payload_source);
+			curl_easy_setopt(_connection, CURLOPT_READDATA, &upload_ctx);
+			curl_easy_setopt(_connection, CURLOPT_UPLOAD, 1L);
+
+			res = curl_easy_perform(_connection);
+
+            //Stringdata textdata{ SetPayloadText };
+
+			// 결과 확인
+			if (res != CURLE_OK)
+			{
+				cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+			}
+			else
+			{
+				cout << "Email sent successfully!" << endl;
+			}
+
+			// 메모리 해제
+			curl_slist_free_all(recipients);
+		}
+    }
+}
+
+Email::Email(const string& toAddr, const string& fromAddr, const string& nameFrom, const string& subject, const string& message, const string& ccAddr)
+{
+    _toAddr = toAddr;
+	_fromAddr = fromAddr;
+	_nameFrom = nameFrom;
+	_subject = subject;
+	_message = message;
+	_ccAddr = ccAddr.empty() ? toAddr : ccAddr;
+}
+
+string Email::DateTimeNow() const
+{
+    const int RFC5322_TIME_LEN = 32;
+
+    std::string ret;
+    ret.resize(RFC5322_TIME_LEN);
+
+    time_t tt;
+
+#ifdef _MSC_VER
+    time(&tt);
+    tm* t = localtime(&tt);
+#else
+    tm tv, * t = &tv;
+    tt = time(&tt);
+    localtime_r(&tt, t);
+#endif
+
+    strftime(&ret[0], RFC5322_TIME_LEN, "%a, %d %b %Y %H:%M:%S %z", t);
+
+    return ret;
+}
+
+CURLcode Email::Send(const string& url, const string& userName, const string& password)
+{
+    CURLcode ret = CURLE_OK;
+
+    struct curl_slist* recipients = NULL;
+
+    CURL* curl = curl_easy_init();
+
+    StringData textData{ SetPayloadText() };
+
+    if (curl) 
+    {
+        curl_easy_setopt(curl, CURLOPT_USERNAME, userName.c_str());
+        curl_easy_setopt(curl, CURLOPT_PASSWORD, password.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
+        //curl_easy_setopt(curl, CURLOPT_CAINFO, "/path/to/certificate.pem");
+
+        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, ("<" + _fromAddr + ">").c_str());
+        recipients = curl_slist_append(recipients, ("<" + _toAddr + ">").c_str());
+        recipients = curl_slist_append(recipients, ("<" + _ccAddr + ">").c_str());
+
+        curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+        curl_easy_setopt(curl, CURLOPT_READDATA, &textData);
+        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+        ret = curl_easy_perform(curl);
+
+        if (ret != CURLE_OK) 
+        {
+            cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << "\n";
+        }
+
+        curl_slist_free_all(recipients);
+        curl_easy_cleanup(curl);
+    }
+
+    return ret;
+}
+
+string Email::SetPayloadText()
+{
+    string payloadText = "Date: " + DateTimeNow() + "\r\n"
+        "To: <" + _toAddr + ">\r\n"
+        "From: \"" + _nameFrom + "\" <" + _fromAddr + ">\r\n";
+    if (_ccAddr.length()) {
+        payloadText += "Cc: <" + _ccAddr + ">\r\n";
+    }
+    payloadText += "Message-ID: <" + GenerateMessageID() + "@" + _fromAddr.substr(_fromAddr.find('@') + 1) + ">\r\n"
+        "Subject: " + _subject + "\r\n"
+        "\r\n"
+        + _message + "\r\n"
+        "\r\n";
+    return payloadText;
+}
+
+string Email::GenerateMessageID() const
+{
+    const size_t MESSAGE_ID_LEN = 37;
+    tm t;
+    time_t tt;
+    time(&tt);
+#ifdef _MSC_VER
+    gmtime_s(&t, &tt);
+#else
+    gmtime_r(&tt, &t);
+#endif
+    std::string ret;
+    ret.resize(MESSAGE_ID_LEN);
+    size_t datelen = std::strftime(&ret[0], MESSAGE_ID_LEN, "%Y%m%d%H%M%S", &t);
+    static const std::string alphaNum{
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz" };
+    std::mt19937 gen;
+    std::uniform_int_distribution<> dis(0, alphaNum.length() - 1);
+    std::generate_n(ret.begin() + datelen, MESSAGE_ID_LEN - datelen, [&]() { return alphaNum[dis(gen)]; });
+    return ret;
+}
+
+size_t Email::payload_source(void* ptr, size_t size, size_t nmemb, void* userp)
+{
+    StringData* text = reinterpret_cast<StringData*>(userp);
+
+    if ((size == 0) || (nmemb == 0) || ((size * nmemb) < 1) || (text->bytesleft == 0)) {
+        return 0;
+    }
+
+    if ((nmemb * size) >= text->msg.size()) {
+        text->bytesleft = 0;
+        return text->msg.copy(reinterpret_cast<char*>(ptr), text->msg.size());
+    }
+
+    return 0;
 }
--- Server/GameServer/CURLMananger.h
+++ Server/GameServer/CURLMananger.h
@@ -1,4 +1,48 @@
 #pragma once
+#include <curl/curl.h>
+
+struct AuthInfo
+{
+	string password;
+	int authNo;
+};
+
+class Email
+{
+public:
+	Email(const string& toAddr,
+		const string& fromAddr,
+		const string& nameFrom,
+		const string& subject,
+		const string& message,
+		const string& ccAddr = ""
+	);
+	~Email();
+
+private:
+	struct StringData 
+	{
+		string msg;
+		size_t bytesleft;
+		StringData(string&& m) : msg{ m }, bytesleft{ msg.size() } {}
+		StringData(string& m) = delete;
+	};
+
+	string DateTimeNow() const;
+	CURLcode Send(const string& url, const string& userName, const string& password);
+private:
+	string SetPayloadText();
+	string GenerateMessageID() const;
+	static size_t payload_source(void* ptr, size_t size, size_t nmemb, void* userp);
+private:
+	string _toAddr;
+	string _fromAddr;
+	string _nameFrom;
+	string _subject;
+	string _message;
+	string _ccAddr;
+};
+
 class CURLMananger
 {
 public:
@@ -8,18 +52,27 @@ public:
 public:
 	static void SendMail();
 
+	void SendMail(const string& toAddr, const string& password);
+
 	void SetToAddr(const string& toAddr) { this->_toAddr = toAddr; }
 
 	void SetMessageID(const string& messageID) { this->_messageID = messageID; }
 
+	void AddAuthWaiter(const string& email, const string& password);
+
 private:
 	string GenerateMessageID(const std::string& domain);
 
-	string SetPayLoadText();
+	string SetPayLoadText(const string& email, AuthInfo info);
 
-	string GenerateVerificationCode();
+	int GenerateVerificationCode();
 private:
-	string _payload_string;
 	string _toAddr;
 	string _messageID;
+
+private: 
+	USE_LOCK;
+	map<string, AuthInfo> _authWaiters;
+	CURL* _connection;
+	string _payload;
 };
